678172f7d3a5912cf116cdd720a396b42cf25886
Suniganti-srija <srijasuniganti1@gmail.com>
2025-05-21 23:37:02 +0530

    2 files on access modifiers

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package com.bean;
+
+	public class Employee {
+			private int id;
+			float salary;
+			protected String name;
+	public void Displayemp() {
+		System.out.println("employee id is"+id);
+		System.out.println("employee salary is"+salary);
+		System.out.println("employee name is"+name);
+	}
+		
+	}
+
+
---
+++ EmployeeTest.java
@@ -0,0 +1,17 @@
+package com.main;
+	import com.bean.Employee;
+
+	public class EmployeeTest {
+		public static void main(String [] args) {
+			Employee emp = new Employee();
+			emp.Displayemp();
+			
+				
+			
+		}
+		
+
+	}
+
+
+
